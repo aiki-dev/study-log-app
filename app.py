a820d60cba96e932400037c28a652c1462fc877c
aiki-dev <aiki74cr230084.adb@gmail.com>
2026-03-12 07:32:02 +0900

    学習記録アプリに入力バリデーションと日付・科目入力機能を追加

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import csv
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -7,18 +8,39 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def home():
     message = ""
+    error = ""
 
     if request.method == "POST":
+        study_date = request.form["study_date"]
+        subject = request.form["subject"]
         study_time = request.form["study_time"]
 
-        # CSVに追記保存（過去データを消さない）
-        with open("study_log.csv", "a", newline="", encoding="utf-8") as file:
-            writer = csv.writer(file)
-            writer.writerow([study_time])
+        #　空欄チェック
+        if not study_date or not subject or not study_time:
+            error = "すべての項目を入力してください。"
+        
+        else:
+            try:
+                datetime.strptime(study_date, "%Y-%m-%d")
+            except ValueError:
+                error = "正しい日付を入力してください。"
+            
+            if error == "":
+                if not study_time.isdigit():
+                    error = "学習時間は整数で入力してください。"
+                else:
+                    study_time_int = int(study_time)
+                    if study_time_int < 1 or study_time_int > 1440:
+                        error = "学習時間は1~1440の範囲で入力してください。"
+            if error == "":
+                # CSVに追記保存（過去データを消さない）
+                with open("study_log.csv", "a", newline="", encoding="utf-8") as file:
+                    writer = csv.writer(file)
+                    writer.writerow([study_date, subject, study_time])
 
-        message = f"あなたは {study_time} 時間勉強しました"
+                message = f"{study_date} / {subject} / {study_time}分 を保存しました。"
 
-    return render_template("index.html", msg=message)
+    return render_template("index.html", msg=message, error=error)
 
 #　このファイルを直接実行した時のみアプリ起動
 if __name__ == "__main__":
