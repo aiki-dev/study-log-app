9c56af9c23da2cfb4de3c6625d0525cc838bba00
aiki-dev <aiki74cr230084.adb@gmail.com>
2026-03-25 15:30:26 +0900

    pytestでテスト2件を追加してコメントも追記

--- tests/test_app.py
+++ tests/test_app.py
@@ -32,3 +32,21 @@ def test_blank_date_error(client):
         "study_time": "60"
     })
     assert "すべての項目を入力してください。" in response.data.decode("utf-8")
+
+# 学習時間に文字を入れたときにエラーが出るか確認
+def test_string_time_error(client):
+    response = client.post("/", data={
+        "study_date": "2026-03-01",
+        "subject": "数学",
+        "study_time": "abc"
+    })
+    assert "学習時間は整数で入力してください。" in response.data.decode("utf-8")
+
+# 学習時間に0を入れたときにエラーが出るか確認
+def test_zero_time_error(client):
+    response = client.post("/", data={
+        "study_date": "2026-03-01",
+        "subject": "数学",
+        "study_time": "0"
+    })
+    assert "学習時間は1~1440の範囲で入力してください。" in response.data.decode("utf-8")
